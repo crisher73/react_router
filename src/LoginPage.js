aabf0fc0e773224d1c846a6192e31c9b28df00db
Cristina Hernández <achh17@gmail.com>
2023-03-23 16:31:36 -0500

    I added Navigate and Redirects

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 import { useAuth } from './auth';
+
 
 function LoginPage() {
   const auth = useAuth();
@@ -9,6 +11,10 @@ function LoginPage() {
     e.preventDefault();
     auth.login({ username });
   };
+
+  if(auth.user) {
+    return <Navigate to='/profile'/>
+  }
   
   return (
     <>
